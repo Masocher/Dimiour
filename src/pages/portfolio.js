f8166b996c3f7ed7cb538a9d12090d496e7c64d2
Masocher <Masochergamer@gmail.com>
2023-05-06 03:25:58 +0430

    Create Portfolio Page

---
+++ src/pages/portfolio.js
@@ -0,0 +1,18 @@
+// components
+import Header from "../components/header"
+import TopSection from '../components/top-section'
+import ContactUs from '../components/contact-us'
+import Footer from '../components/footer'
+
+const Portfolio = () => {
+    return (
+        <div>
+            <Header/>
+            <TopSection title="Portfolio" issue="Portfolio"/>
+            <ContactUs/>
+            <Footer/>
+        </div>
+    )
+}
+
+export default Portfolio
