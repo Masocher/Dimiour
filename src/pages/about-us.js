8048cffade552978952f3178d6194b9d177ce3a2
Masocher <Masochergamer@gmail.com>
2023-05-05 21:46:51 +0430

    Create About-Us Page

---
+++ src/pages/about-us.js
@@ -0,0 +1,23 @@
+// assets
+import styles from '../styles/pages/About-us.module.css'
+
+// components
+import Header from '../components/header'
+import TopSection from '../components/top-section'
+import AboutSection from '../components/about-section'
+import AboutSection2 from '../components/about-section-2'
+import Footer from '../components/footer'
+
+const About = () => {
+    return (
+        <div className={styles.container}>
+            <Header/>
+            <TopSection title="ABOUT US" issue="About"/>
+            <AboutSection/>
+            <AboutSection2/>
+            <Footer/>
+        </div>
+    )
+}
+
+export default About
