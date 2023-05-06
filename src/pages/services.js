8e10f8e117800cc252ded1a1f99951f0cb69d8b7
Masocher <Masochergamer@gmail.com>
2023-05-06 16:42:25 +0430

    Create Services Page

---
+++ src/pages/services.js
@@ -0,0 +1,19 @@
+// components
+import Header from "../components/header"
+import TopSection from '../components/top-section'
+import ContactUs from '../components/contact-us'
+import Footer from '../components/footer'
+
+
+const Services = () => {
+    return (
+        <div>
+            <Header/>
+            <TopSection title="OUR SERVICES" issue="Services"/>
+            <ContactUs/>
+            <Footer/>
+        </div>
+    )
+}
+
+export default Services
