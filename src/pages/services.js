e59a7d8e53770891b33ff0325de4a4d65fa23f3b
Masocher <Masochergamer@gmail.com>
2023-05-06 17:13:51 +0430

    Responsive The Services Page's More-Services Section

--- src/pages/services.js
+++ src/pages/services.js
@@ -1,6 +1,8 @@
 // components
 import Header from "../components/header"
 import TopSection from '../components/top-section'
+import FeaturedServices from '../components/featured-services'
+import MoreServices from "../components/more-services"
 import ContactUs from '../components/contact-us'
 import Footer from '../components/footer'
 
@@ -10,6 +12,8 @@ const Services = () => {
         <div>
             <Header/>
             <TopSection title="OUR SERVICES" issue="Services"/>
+            <FeaturedServices/>
+            <MoreServices/>
             <ContactUs/>
             <Footer/>
         </div>
