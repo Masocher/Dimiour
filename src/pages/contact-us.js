e29d387a82efe113e93d5a45e73e275d35434b96
Masocher <Masochergamer@gmail.com>
2023-05-06 15:41:29 +0430

    Create Contact Page

---
+++ src/pages/contact-us.js
@@ -0,0 +1,16 @@
+// components
+import Header from "../components/header"
+import TopSection from "../components/top-section"
+import Footer from "../components/footer"
+
+const ContactUs = () => {
+    return (
+        <div>
+            <Header/>
+            <TopSection title="CONTACT US" issue="Contact"/>
+            <Footer/>
+        </div>
+    )
+}
+
+export default ContactUs
