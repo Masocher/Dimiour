bc31951849b7d79054adae944d2bc8da8b134bf5
Masocher <Masochergamer@gmail.com>
2023-05-07 16:54:34 +0430

    Complete The Single-Portfolio Page

--- src/pages/single-portfolio.js
+++ src/pages/single-portfolio.js
@@ -1,6 +1,7 @@
 // components
 import Header from '../components/header'
 import TopSection from '../components/top-section'
+import SingleProject from '../components/single-project'
 import Testimonials from '../components/testimonials'
 import ContatcUs from '../components/contact-us'
 import Footer from '../components/footer'
@@ -10,6 +11,7 @@ const SinglePortfolio = () => {
         <div>
             <Header/>
             <TopSection title="PROJECT TITLE HERE" issue="Project Title Here" status={true} />
+            <SingleProject/>
             <Testimonials/>
             <ContatcUs/>
             <Footer/>
