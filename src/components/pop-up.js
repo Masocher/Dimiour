6f47ea621ecdb60a382953352e8195dade55ee55
Masocher <Masochergamer@gmail.com>
2023-05-08 06:48:39 +0430

    Complete The Dimiour

--- src/components/pop-up.js
+++ src/components/pop-up.js
@@ -13,7 +13,6 @@ const PopUp = () => {
     return (
         <div className={styles.container} style={{ display: status ? "block" : "none" }} onMouseEnter={() => dispatch(openPopUp())} onMouseLeave={() => dispatch(closePopUp())}>
             <div className={styles.pop_up_box}>
-                <div className={styles.triangle}></div>
                 <div className={styles.link}>Branding</div>
                 <div className={styles.link}>Web Design</div>
                 <div className={styles.link}>App Design</div>
